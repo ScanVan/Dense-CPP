324ce67ae5df83e81b61d13e7edd2ed4e47ba3e5
nils-hamel <nils.hamel@bluewin.ch>
2019-09-13 15:01:22 +0200

    Changing disparity condition to use angles as criterion

--- src/Dense.cpp
+++ src/Dense.cpp
@@ -417,47 +417,43 @@
 
     void sv_dense_filter( double const sv_tol, double const sv_ang, std::vector < Eigen::Vector3d > const & sv_scene, std::vector < Eigen::Vector3i > const & sv_color, std::vector < Eigen::Vector3d > & sv_fscene, std::vector < Eigen::Vector3i > & sv_fcolor, std::vector < Eigen::Vector3d > const & sv_mat_1, std::vector < Eigen::Vector3d > const & sv_mat_2, std::vector < Eigen::Vector3d > const & sv_mat_3, Eigen::Vector3d const & sv_cen_1, Eigen::Vector3d const & sv_cen_2, Eigen::Vector3d const & sv_cen_3 ) {
 
-        /* element radius variable */
-        double sv_rad_1( 0.0 );
-        double sv_rad_2( 0.0 );
-        double sv_rad_3( 0.0 );
-
-        /* element angle variable */
-        double sv_ang_12( 0.0 );
-        double sv_ang_23( 0.0 );
-        double sv_ang_31( 0.0 );
+        /* direction vector variable */
+        Eigen::Vector3d sv_vec_1;
+        Eigen::Vector3d sv_vec_2;
+        Eigen::Vector3d sv_vec_3;
 
         /* element disparity variable */
         double sv_disp_1( 0.0 );
         double sv_disp_2( 0.0 );
         double sv_disp_3( 0.0 );
 
+        /* vector norm variable */
         double sv_norm_1( 0.0 );
         double sv_norm_2( 0.0 );
         double sv_norm_3( 0.0 );
 
+        /* element angle variable */
+        double sv_ang_12( 0.0 );
+        double sv_ang_23( 0.0 );
+        double sv_ang_31( 0.0 );
+
         /* parsing scene elements */
         for ( long sv_parse( 0 ); sv_parse < sv_scene.size(); sv_parse ++ ) {
 
-            /* compute element radius */
-            sv_rad_1 = sv_mat_1[sv_parse].transpose() * ( sv_scene[sv_parse] - sv_cen_1 );
-            sv_rad_2 = sv_mat_2[sv_parse].transpose() * ( sv_scene[sv_parse] - sv_cen_2 );
-            sv_rad_3 = sv_mat_3[sv_parse].transpose() * ( sv_scene[sv_parse] - sv_cen_3 );
+            /* compute direction to optimal point */
+            sv_vec_1 = sv_scene[sv_parse] - sv_cen_1;
+            sv_vec_2 = sv_scene[sv_parse] - sv_cen_2;
+            sv_vec_3 = sv_scene[sv_parse] - sv_cen_3;
 
-            /* compute element disparity */
-            sv_disp_1 = ( sv_cen_1 + ( sv_rad_1 * sv_mat_1[sv_parse] ) - sv_scene[sv_parse] ).norm();
-            sv_disp_2 = ( sv_cen_2 + ( sv_rad_2 * sv_mat_2[sv_parse] ) - sv_scene[sv_parse] ).norm();
-            sv_disp_3 = ( sv_cen_3 + ( sv_rad_3 * sv_mat_3[sv_parse] ) - sv_scene[sv_parse] ).norm();
-
-            /* compute norms */
-            sv_norm_1 = sv_mat_1[sv_parse].norm();
-            sv_norm_2 = sv_mat_2[sv_parse].norm();
-            sv_norm_3 = sv_mat_3[sv_parse].norm();
+            /* compute disparity angles */
+            sv_disp_1 = acos( ( sv_vec_1.dot( sv_mat_1[sv_parse] ) ) / sv_vec_1.norm() );
+            sv_disp_2 = acos( ( sv_vec_2.dot( sv_mat_2[sv_parse] ) ) / sv_vec_2.norm() );
+            sv_disp_3 = acos( ( sv_vec_3.dot( sv_mat_3[sv_parse] ) ) / sv_vec_3.norm() );
 
             /* compute element angle */
-            sv_ang_12 = acos( sv_mat_1[sv_parse].dot( sv_mat_2[sv_parse] ) / ( sv_norm_1 * sv_norm_2 ) );
-            sv_ang_23 = acos( sv_mat_2[sv_parse].dot( sv_mat_3[sv_parse] ) / ( sv_norm_2 * sv_norm_3 ) );
-            sv_ang_31 = acos( sv_mat_3[sv_parse].dot( sv_mat_1[sv_parse] ) / ( sv_norm_3 * sv_norm_1 ) );
+            sv_ang_12 = acos( sv_mat_1[sv_parse].dot( sv_mat_2[sv_parse] ) );
+            sv_ang_23 = acos( sv_mat_2[sv_parse].dot( sv_mat_3[sv_parse] ) );
+            sv_ang_31 = acos( sv_mat_3[sv_parse].dot( sv_mat_1[sv_parse] ) );
 
             /* apply filtering condition */
             if ( sv_disp_1 <= sv_tol )
@@ -467,16 +463,11 @@
             /* apply filtering condition */
             if ( ( sv_ang_12 > sv_ang ) || ( sv_ang_23 > sv_ang ) || ( sv_ang_31 > sv_ang ) ) {
 
-                /* apply filtering condition */
-                if ( sv_rad_2 > 0.0 ) {
+                /* element selection - position */
+                sv_fscene.push_back( sv_scene[sv_parse] );
 
-                    /* element selection - position */
-                    sv_fscene.push_back( sv_scene[sv_parse] );
-
-                    /* element selection - color */
-                    sv_fcolor.push_back( sv_color[sv_parse] );
-
-                }
+                /* element selection - color */
+                sv_fcolor.push_back( sv_color[sv_parse] );
 
             }
 
@@ -485,6 +476,7 @@
         }
 
     }
+
 
 /*
     source - main function
@@ -606,7 +598,7 @@
         sv_tol = sv_dense_geometry_amplitude( sv_cen_1, sv_cen_2, sv_cen_3 ) / 150.0;
 
         /* filter scene */
-        sv_dense_filter( sv_tol, 1.0 * ( 3.1415926535 / 180.0 ), sv_scene, sv_color, sv_fscene, sv_fcolor, sv_mat_1, sv_mat_2, sv_mat_3, sv_cen_1, sv_cen_2, sv_cen_3 );
+        sv_dense_filter( ( ( 2.0 * 3.1415926535 ) / sv_img_width ) * 0.2, ( 3.1415926535 / 180.0 ) * 1.5, sv_scene, sv_color, sv_fscene, sv_fcolor, sv_mat_1, sv_mat_2, sv_mat_3, sv_cen_1, sv_cen_2, sv_cen_3 );
 
         /* export computed scene */
         sv_dense_io_scene( argv[5], sv_fscene, sv_fcolor );
